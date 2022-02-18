8f9c0239c33d121e419af73355acafebdf08394f
Adria Ivanitsky <littleivanitsky@gmail.com>
2022-02-18 10:15:31 -0800

    got delete function working

--- src/components/GroceryList.jsx
+++ src/components/GroceryList.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import './GroceryList.css';
-export default function GroceryList({ items }) {
+
+export default function GroceryList({ items, onDeleteItem }) {
   return (
     <div>
       {items.map((item) => (
         <label key={item.id}>
           <input type="checkbox"></input>
           {item.text}
+          <button onClick={() => onDeleteItem(item.id)}>delete</button>
         </label>
       ))}
     </div>
