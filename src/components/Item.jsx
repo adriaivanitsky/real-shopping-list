f9287c57b29b2334776271575297758e539aaf1c
Adria Ivanitsky <littleivanitsky@gmail.com>
2022-02-18 17:01:47 -0800

    got edit button to render and halfway work

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -1,10 +1,28 @@
-import React from 'react';
+import { useState } from 'react';
 
 export default function Item({ item, onDeleteItem }) {
+  const [edit, setEdit] = useState(false);
+  let itemContent;
+
+  if (edit) {
+    itemContent = (
+      <>
+        <input value={item.text} />
+        <button onClick={() => setEdit(false)}>save</button>
+      </>
+    );
+  } else {
+    itemContent = (
+      <>
+        <span>{item.text}</span>
+        <button onClick={() => setEdit(true)}>edit</button>
+      </>
+    );
+  }
   return (
     <>
       <input type="checkbox"></input>
-      {item.text}
+      {itemContent}
       <button onClick={() => onDeleteItem(item.id)}>delete</button>
     </>
   );
